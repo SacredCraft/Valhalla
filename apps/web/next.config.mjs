1115f71f6e29ac995698c68ce47059d4d595877b
LioRael <621862169@qq.com>
2024-08-16 12:28:07 +0800

    chore: Remove MillionLint configuration from next.config.mjs

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,8 +1,4 @@
-import MillionLint from "@million/lint";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default MillionLint.next({
-  rsc: true,
-})(nextConfig);
+export default nextConfig;
