d2638cdc3742d4c937d2f2ff0c6a03c1b8c07738
LioRael <621862169@qq.com>
2024-08-27 04:58:42 +0800

    refactor: Update search route to use createSearchAPI`

--- apps/docs/app/api/search/route.ts
+++ apps/docs/app/api/search/route.ts
@@ -1,18 +1,12 @@
-import { createI18nSearchAPI } from "fumadocs-core/search/server";
+import { createSearchAPI } from "fumadocs-core/search/server";
 
-import { languages } from "@/utils/i18n";
 import { getPages } from "@/utils/source";
 
-export const { GET } = createI18nSearchAPI("advanced", {
-  indexes: languages.map((lang) => {
-    return {
-      language: lang,
-      indexes: getPages(lang)!.map((page) => ({
-        id: page.url,
-        url: page.url,
-        title: page.data.title,
-        structuredData: page.data.exports.structuredData,
-      })),
-    };
-  }),
+export const { GET } = createSearchAPI("advanced", {
+  indexes: getPages()!.map((page) => ({
+    id: page.url,
+    url: page.url,
+    title: page.data.title,
+    structuredData: page.data.exports.structuredData,
+  })),
 });
