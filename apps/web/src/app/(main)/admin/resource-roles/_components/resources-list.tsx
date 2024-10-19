30286f655981561819e3ff10cb63bccdfc7e89ab
LioRael <621862169@qq.com>
2024-10-19 12:32:54 +0800

    refactor: 重构资源列表组件并添加资源搜索功能

--- apps/web/src/app/(main)/admin/resource-roles/_components/resources-list.tsx
+++ apps/web/src/app/(main)/admin/resource-roles/_components/resources-list.tsx
@@ -1,66 +1,162 @@
-import React from "react";
+import React, { Dispatch, SetStateAction, useState } from "react";
 
 import valhallaConfig from "@/config";
 import { useControllableState } from "@radix-ui/react-use-controllable-state";
 import {
-  Checkbox,
+  Button,
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
   FormDescription,
   FormLabel,
+  Input,
   ScrollArea,
-  cn,
 } from "@sacred-craft/valhalla-components";
 
 type ResourceListProps = {
   selectedResources: string[];
   // eslint-disable-next-line no-unused-vars
   setSelectResources: (resource: string[]) => void;
-  disabled?: boolean;
 };
 
 export const ResourcesList = ({
   selectedResources,
   setSelectResources,
-  disabled,
 }: ResourceListProps) => {
   const [value, setValue] = useControllableState<string[]>({
     prop: selectedResources,
     onChange: setSelectResources,
   });
+  const [search, setSearch] = useState("");
 
   if (!value) {
     return null;
   }
 
   return (
-    <div>
+    <div className="flex flex-col gap-2">
       <FormLabel>Resources</FormLabel>
-      <ScrollArea className="-m-2 my-1">
-        {valhallaConfig.resources.map((resource, index) => (
-          <li
-            key={index}
-            className={cn("p-2 flex items-center gap-2 rounded-md", {
-              "opacity-50": disabled,
-            })}
-          >
-            {selectedResources.includes(resource.name)}
-            <Checkbox
-              checked={value.includes(resource.name)}
-              onCheckedChange={(checked) =>
-                setValue(
-                  checked
-                    ? [...value, resource.name]
-                    : value.filter((v) => v !== resource.name),
-                )
-              }
-              disabled={disabled}
-            />
-            <span className="text-sm font-medium">{resource.name}</span>
-          </li>
-        ))}
+      <div className="flex items-center gap-2">
+        <Input
+          placeholder="Search resources"
+          className="h-8"
+          value={search}
+          onChange={(value) => setSearch(value as string)}
+        />
+        <AddResource setResources={setValue} ids={value} />
+      </div>
+      <ScrollArea className="flex flex-col gap-2 border rounded-md p-2">
+        {selectedResources.length > 0 ? (
+          <div className="flex flex-col gap-2">
+            {selectedResources.map((resource, index) => (
+              <li key={index} className="flex items-center justify-between">
+                <div className="flex items-center gap-2">
+                  <span className="text-sm">{resource}</span>
+                </div>
+                <Button
+                  size="sm"
+                  variant="outline"
+                  onClick={() => {
+                    setValue(value.filter((v) => v !== resource));
+                  }}
+                >
+                  Remove
+                </Button>
+              </li>
+            ))}
+          </div>
+        ) : (
+          <div className="text-sm text-muted-foreground min-h-20 flex items-center justify-center">
+            No resources found
+          </div>
+        )}
       </ScrollArea>
       <FormDescription>
         Select the resources that should be associated with this role.
       </FormDescription>
     </div>
+  );
+};
+
+export const AddResource = ({
+  ids,
+  setResources,
+}: {
+  ids: string[] | undefined;
+  setResources: Dispatch<SetStateAction<string[] | undefined>>;
+}) => {
+  const [search, setSearch] = useState("");
+
+  const resources = valhallaConfig.resources.filter((resource) =>
+    resource.name.includes(search),
+  );
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline" size="sm">
+          Add Resource
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Add Resource</DialogTitle>
+          <DialogDescription>
+            Search for a resource to add to this role.
+          </DialogDescription>
+        </DialogHeader>
+        <Input
+          placeholder="Search resources"
+          className="h-8"
+          value={search}
+          onChange={(value) => setSearch(value as string)}
+        />
+        <ScrollArea className="border rounded-md p-2">
+          {resources && resources.length > 0 ? (
+            <div className="flex flex-col gap-2">
+              {resources.map((resource, index) => (
+                <li key={index} className="flex items-center justify-between">
+                  <div className="flex items-center gap-2">
+                    <span className="text-sm">{resource.name}</span>
+                  </div>
+                  {ids?.includes(resource.name) ? (
+                    <Button
+                      size="sm"
+                      variant="outline"
+                      onClick={() => {
+                        setResources((prev) =>
+                          prev?.filter((id) => id !== resource.name),
+                        );
+                      }}
+                    >
+                      Remove
+                    </Button>
+                  ) : (
+                    <Button
+                      size="sm"
+                      onClick={() => {
+                        setResources((prev) => [
+                          ...(prev ?? []),
+                          resource.name,
+                        ]);
+                      }}
+                    >
+                      Add
+                    </Button>
+                  )}
+                </li>
+              ))}
+            </div>
+          ) : (
+            <div className="text-sm text-muted-foreground min-h-20 flex items-center justify-center">
+              No resources found
+            </div>
+          )}
+        </ScrollArea>
+      </DialogContent>
+    </Dialog>
   );
 };
