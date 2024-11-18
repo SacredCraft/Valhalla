f603656039c01cc575d960456855f5555a71a1be
LioRael <621862169@qq.com>
2024-11-18 20:52:46 +0800

    feat: 添加配置脚本以初始化系统配置，更新依赖 yaml 至 2.6.0，移除不再使用的 workspaces 脚本

---
+++ scripts/install.ts
@@ -0,0 +1,53 @@
+import { checkbox, input, select } from '@inquirer/prompts'
+import chalk from 'chalk'
+import fs from 'fs-extra'
+import yaml from 'yaml'
+
+// 读取配置文件
+const config = yaml.parse(fs.readFileSync('configs/system.yaml', 'utf8'))
+
+let updates: any = {
+  ...config,
+}
+
+if (!config?.database) {
+  updates.database = await input({
+    message: '请输入数据库 URL',
+  })
+}
+
+if (!config?.['better-auth']?.secret) {
+  updates['better-auth'] = {
+    secret: await input({
+      message: '请输入 Better Auth 的 API 密钥 (留空则自动生成)',
+      default: Array.from(crypto.getRandomValues(new Uint8Array(16)))
+        .map((b) => {
+          const chars =
+            '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+          return (
+            chars[b % chars.length] +
+            chars[Math.floor(b / chars.length) % chars.length]
+          )
+        })
+        .join(''),
+    }),
+  }
+}
+
+if (!config?.users?.avatar) {
+  console.log(chalk.blue('检测到未配置头像，将使用默认配置'))
+  updates.users = {
+    avatar: {
+      'max-size': 2048,
+      accept: 'image/*',
+      width: 200,
+      height: 200,
+      path: './files/avatars',
+      quality: 80,
+    },
+  }
+}
+
+fs.writeFileSync('configs/system.yaml', yaml.stringify(updates))
+
+console.log(chalk.green('配置文件已保存'))
