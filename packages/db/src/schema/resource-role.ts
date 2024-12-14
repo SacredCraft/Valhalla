cc927981dcfab9cf8a669f48b3b41602739e3db0
LioRael <22147493@qq.com>
2024-12-14 19:17:25 +0800

    feat: add resource-role exports and enhance relationships in schema
    - Added export for resource-role schema to index file, improving accessibility.
    - Introduced new relationships for resourceRoleResource and userResourceRole, enhancing data integrity and management capabilities.
    - Updated resource-role schema to better define relationships between resources, users, and resource roles, streamlining user management processes.

--- packages/db/src/schema/resource-role.ts
+++ packages/db/src/schema/resource-role.ts
@@ -21,8 +21,19 @@ export const resourceRoleResource = pgTable('resource_role_resource', {
   resourceName: text('resourceName').notNull(),
 })
 
+export const resourceRoleResourceRelations = relations(
+  resourceRoleResource,
+  ({ one }) => ({
+    resourceRole: one(resourceRole, {
+      fields: [resourceRoleResource.resourceRoleId],
+      references: [resourceRole.id],
+    }),
+  })
+)
+
 export const resourceRoleRelations = relations(resourceRole, ({ many }) => ({
   resources: many(resourceRoleResource),
+  users: many(userResourceRole),
 }))
 
 export const userResourceRole = pgTable('user_resource_role', {
@@ -33,3 +44,17 @@ export const userResourceRole = pgTable('user_resource_role', {
     .notNull()
     .references(() => resourceRole.id),
 })
+
+export const userResourceRoleRelations = relations(
+  userResourceRole,
+  ({ one }) => ({
+    user: one(user, {
+      fields: [userResourceRole.userId],
+      references: [user.id],
+    }),
+    resourceRole: one(resourceRole, {
+      fields: [userResourceRole.resourceRoleId],
+      references: [resourceRole.id],
+    }),
+  })
+)
